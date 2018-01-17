3bf18f46289f710894629bda751923408f5176b8
weisu00@gmail.com <weisu00@gmail.com>
2018-01-16 21:43:28 -0600

    update3

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -1,4 +1,25 @@
 package io.pivotal.pal.tracker;
 
+import java.util.List;
+
 public class InMemoryTimeEntryRepository extends TimeEntryRepository {
+
+    public TimeEntry create(TimeEntry timeEntry) {
+        return null;
+    }
+
+    public TimeEntry find(Object id) {
+        return null;
+    }
+
+    public List<TimeEntry> list() {
+        return null;
+    }
+
+    public TimeEntry update(Object id, TimeEntry timeEntry) {
+        return null;
+    }
+
+    public void delete(Object id) {
+    }
 }
--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -2,23 +2,14 @@ package io.pivotal.pal.tracker;
 
 import java.util.List;
 
-public class TimeEntryRepository {
-    public TimeEntry create(TimeEntry timeEntry) {
-        return null;
-    }
+public interface TimeEntryRepository {
+    public TimeEntry create(TimeEntry timeEntry);
 
-    public TimeEntry find(Object id) {
-        return null;
-    }
+    public TimeEntry find(Object id) ;
 
-    public List<TimeEntry> list() {
-        return null;
-    }
+    public List<TimeEntry> list() ;
 
-    public TimeEntry update(Object id, TimeEntry timeEntry) {
-        return null;
-    }
+    public TimeEntry update(Object id, TimeEntry timeEntry) ;
 
-    public void delete(Object id) {
-    }
+    public void delete(Object id) ;
 }
