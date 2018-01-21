f31ee6ad5d5e82338dc1e1714040adc945d25ae7
weisu00 <weisu00@gmail.com>
2018-01-21 14:23:35 -0600

    Update TimeEntryApiTest.java

--- src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
@@ -100,6 +100,7 @@ public class TimeEntryApiTest {
         assertThat(updateJson.read("$.userId", Long.class)).isEqualTo(3L);
         assertThat(updateJson.read("$.date", String.class)).isEqualTo("2017-01-09");
         assertThat(updateJson.read("$.hours", Long.class)).isEqualTo(9);
+        // test
     }
 
     @Test
